bc16dad0c08440393cb80c257a734171be01f4f9
murali <muralimano86@gmail.com>
2024-02-16 20:26:02 +0000

    Extracting temperature and sky conditions from API data

--- backend.py
+++ backend.py
@@ -2,12 +2,20 @@ import requests
 
 API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
 
-def get_data(place, forecast_days=None, kind=None):
+
+def get_data(place, forecast_days, kind):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = forecast_days * 8
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 
-if __name__=="__main__":
-    print(get_data("Tokyo"))
+if __name__ == "__main__":
+    print(get_data("Chennai", 2, "Sky"))
